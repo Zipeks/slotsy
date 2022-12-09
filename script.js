88a67e9a7e504a5df3d668b70745e629db1bb0e7
Mateusz Zieliński <matzielinski7@gmail.com>
2022-12-09 20:13:25 +0100

    v0.2

--- script.js
+++ script.js
@@ -1,51 +1,71 @@
-const slot1 = document.querySelector("#slot1");
-const slot2 = document.querySelector("#slot2");
-const slot3 = document.querySelector("#slot3");
 const points = document.querySelector("#punkty")
-const btnGraj = document.querySelector("#btnGraj");
+const btnGraj = document.querySelector(".btnGraj");
 
-let obrazki = ["img/banan.png","img/sliwka.png","img/gruszka.png","img/truskawka.png"];
+// let obrazki = ["🍍","🍇","🍋","🍎","🫐","🥥","🥕"];
 
-function los() {
-    return Math.round(Math.random()*(obrazki.length-1));
-}
+
+// function los() {
+//     return Math.round(Math.random()*(obrazki.length-1));
+// }
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-
-slot1.src = obrazki[los()];
-slot2.src = obrazki[los()];
-slot3.src = obrazki[los()];
-
 let punkty = 1000;
 points.innerText  = punkty;
 
-btnGraj.addEventListener("click", async () => {
-    if (btnGraj.innerText == "GRAJ") {
-        btnGraj.innerText = "STOP";
-        while (btnGraj.innerText != "GRAJ") {
-            await sleep(100);
-            slot1.dataset.wylosowany = los()
-            slot1.src = obrazki[slot1.dataset.wylosowany];
-            await sleep(100);
-            slot2.dataset.wylosowany = los()
-            slot2.src = obrazki[slot2.dataset.wylosowany];
-            await sleep(100);
-            slot3.dataset.wylosowany = los()
-            slot3.src = obrazki[slot3.dataset.wylosowany];
-        }
-    } else {
-        btnGraj.innerText = "GRAJ";
-        await sleep(500);
-        slot1.src = obrazki[los()];
-        await sleep(100);
-        slot2.src = obrazki[los()];
-        await sleep(100);
-        slot3.src = obrazki[los()];
-        if ((slot1.src === slot2.src) && (slot2.src === slot3.src)) {
-            console.log("LOL");
-        }
+class JednorekiBandyta {
+
+    constructor() {
+        this.obrazki = ["🍍","🍇","🍋","🍎","🫐","🥥","🥕"];
+        this.pola = [document.querySelector("#slot1"),document.querySelector("#slot2"),document.querySelector("#slot3")]
+
+        this.pola[0].innerHTML = this.obrazki[this.los()];
+        this.pola[1].innerHTML = this.obrazki[this.los()];
+        this.pola[2].innerHTML = this.obrazki[this.los()];
     }
-    
+
+    los() {
+        return Math.round(Math.random()*(this.obrazki.length-1));
+    }
+
+    async spuszczenieDzwigni(time) {
+        for (let i = 0 ; i < 3; i++) {
+            
+            await sleep(time);
+
+            let a = this.los();
+            
+            while (this.pola[i].innerHTML == this.obrazki[a]) {
+                a = this.los();
+            }
+            this.pola[i].innerHTML = this.obrazki[a];
+        }        
+    }
+
+    async graj() {
+        if (btnGraj.innerText == "GRAJ") {
+            btnGraj.classList.add("btnActive");
+            btnGraj.innerText = "STOP";
+            while (btnGraj.innerText != "GRAJ") {
+                await this.spuszczenieDzwigni(100)
+            }
+        } else {
+            btnGraj.classList.remove("btnActive");
+            btnGraj.innerText = "GRAJ";
+            btnGraj.disabled = true;
+            await this.spuszczenieDzwigni(500);
+            btnGraj.disabled = false;
+
+            if ((slot1.src === slot2.src) && (slot2.src === slot3.src)) {
+                console.log("LOL");
+            }
+        }        
+    }
+
+}
+
+let bandyta = new JednorekiBandyta();
+btnGraj.addEventListener("click",  () => {
+   bandyta.graj(); 
 })
